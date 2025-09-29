b37b4a016f88bfe8e5e9bdfa40ad78084cf7d9e7
Akshay <224726990+Ritesh-netzoptimize@users.noreply.github.com>
2025-09-29 12:40:43 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+if (import.meta.env.VITE_ENV === "production") {
+  console.log = () => {};
+  console.error = () => {};
+  console.debug = () => {};
+  console.warn = () => {};
+}
+
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
